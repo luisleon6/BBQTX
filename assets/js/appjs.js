a84841ef411aa1d1ec95fc91666874a26a01bfdc
FarahHadid <j.luisleon6@gmail.com>
2022-03-08 19:43:04 -0600

    Adding login screen local storage

--- assets/js/appjs.js
+++ assets/js/appjs.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
-    $('#box-form').submit(function(){
-        var txt_user = $('#txt-user').val();
-        var txt_password = $('#txt-password').val();
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
 
-        //almacenar los valores dentro de localstorage
-        localStorage.setItem('usuario',txt_user);
-        localStorage.setItem('password',txt_password);
-    });
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
 
-    var txt_user = localStorage.getItem('usuario');
-    var txt_password =localStorage.getItem('password');
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
 
-    if(txt_user != null && txt_user != 'undefined'){
-        //llamamos al documento sesion.html
-        window.location = 'sesion.html';
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        alert("Account Created.\n\nPlease Sign In using the link below.");
     }
-});
+    else{
+        alert("Ooopppssss... Duplicate found!!!\nYou have already sigjned up");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        location.href = "/";
+    }
+    e.preventDefault();
+}
